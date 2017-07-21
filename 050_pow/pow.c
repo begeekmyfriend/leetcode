d0fe9c1e6f2f3c9709ea2cf6acc6b499b7290a39
begeekmyfriend <begeekmyfriend@gmail.com>
2017-07-21 10:33:39 +0800

    Pow
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- 050_pow/pow.c
+++ 050_pow/pow.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
+/*
+ * n == 2(10) -- 2 -- res = 1 * 2^2
+ * n == 3(11) -- 2 + 1 -- res = 1 * 2^1 * 2^2
+ * n == 4(100) -- 4 -- res = 1 * 2^4
+ * n == 5(101) -- 4 + 1 -- res = 1 * 2^1 * 2^4
+ * n == 6(110) -- 4 + 2 + 1 -- res = 1 * 2^1 * 2^2 * 2^4
+ */
 static double my_pow(double x, int n)
 {
+    int orig = x;
     int sign = 0;
+    int one_more = 0;
     if (n < 0) {
-        n = -n;
+        if (n == INT_MIN) {
+            n = INT_MAX;
+            one_more = 1;
+        } else {
+            n = -n;
+        }
         sign = 1;
     }
 
@@ -16,6 +31,9 @@ static double my_pow(double x, int n)
         }
         x *= x;
         n >>= 1;
+    }
+    if (one_more) {
+        res *= orig;
     }
 
     return sign ? 1 / res : res;
