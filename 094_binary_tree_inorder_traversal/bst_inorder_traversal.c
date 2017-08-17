4e051f3057d71878f76dacf5b59d905ea60745f2
begeekmyfriend <begeekmyfriend@gmail.com>
2017-08-17 10:09:00 +0800

    BST inorder traversal
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

---
+++ 094_binary_tree_inorder_traversal/bst_inorder_traversal.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ struct TreeNode {
+     int val;
+     struct TreeNode *left;
+     struct TreeNode *right;
+ };
+
+static void traverse(struct TreeNode *node, int *result, int *count)
+{
+    if (node->left != NULL) {
+        traverse(node->left, result, count);
+    }
+    
+    result[*count] = node->val;
+    (*count)++;
+    
+    if (node->right != NULL) {
+        traverse(node->right, result, count);
+    }    
+}
+
+/**
+ *  * Return an array of size *returnSize.
+ *   * Note: The returned array must be malloced, assume caller calls free().
+ *    */
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    if (root == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
+    int count = 0;
+    int *result = malloc(5000 * sizeof(int));
+    traverse(root, result, &count);
+    *returnSize = count;
+    return result;
+}
+
+int main()
+{
+    int count = 0;
+    inorderTraversal(NULL, &count);
+    return 0;
+}
