995b5e2f96fe30c2256fe813043abe1b15c6c6bf
begeekmyfriend <begeekmyfriend@gmail.com>
2017-09-19 09:49:02 +0800

    Best time to buy and sell stock
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- 122_best_time_to_buy_and_sell_stock_ii/stock.c
+++ 122_best_time_to_buy_and_sell_stock_ii/stock.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 static int maxProfit(int* prices, int pricesSize)
 {
-    if (pricesSize == 0) {
-        return 0;
-    }
-
-    int i, diff = 0, min = prices[0];
+    int i, j, total = 0;
     for (i = 1; i < pricesSize; i++) {
-        if (prices[i] < min) {
-            min = prices[i];
-        } else {
-            diff = prices[i] - min > diff ? prices[i] - min : diff;
-        }
+        total += prices[i] > prices[i - 1] ? prices[i] - prices[i - 1] : 0;
     }
-
-    return diff;
+    return total;
 }
 
 int main(int argc, char **argv)
