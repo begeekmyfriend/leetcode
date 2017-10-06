09e20507ad99e79e0105660c86d9fe848a0b6154
Leo Ma <begeekmyfriend@gmail.com>
2017-10-06 11:49:52 +0800

    Climbing stairs
    Signed-off-by: Leo Ma <begeekmyfriend@gmail.com>

---
+++ 070_climbing_stairs/climb_stairs.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int recursive(int n, int *count)
+{
+    if (n == 0) {
+        return 0;
+    } else if (count[n] > 0) {
+        return count[n];
+    } else {
+        if (n >= 1) {
+            count[n] += recursive(n - 1, count);
+        }
+        if (n >= 2) {
+            count[n] += recursive(n - 2, count);
+        }
+        return count[n];
+    }
+}
+
+static int climbStairs(int n)
+{
+    int *count = malloc((n + 1) * sizeof(int));
+    memset(count, 0, (n + 1) * sizeof(int));
+    count[1] = 1;
+    count[2] = 2;
+    return recursive(n, count);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./test n\n");
+        exit(-1);
+    }
+
+    printf("%d\n", climbStairs(atoi(argv[1])));
+    return 0;
+}
