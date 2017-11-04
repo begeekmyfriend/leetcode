2fde4f6e843522ee2fe1005e5e0ed761b51d6909
Leo Ma <begeekmyfriend@gmail.com>
2017-11-04 09:29:14 +0800

    Water container
    Signed-off-by: Leo Ma <begeekmyfriend@gmail.com>

---
+++ 011_container_with_most_water/container.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int maxArea(int* height, int heightSize)
+{
+    int min = 0, max = heightSize - 1;
+    int area_max = 0;
+    while (min < max) {
+        int area = (max - min) * (height[min] < height[max] ? height[min] : height[max]);
+        area_max = area > area_max ? area : area_max;
+        if (height[min] < height[max]) {
+            while (++min < max && height[min] <= height[min - 1]) {
+                continue;
+            }
+        } else {
+            while (min < --max && height[max] <= height[max + 1]) {
+                continue;
+            }
+        }
+    }
+    return area_max;
+}
+
+int main(int argc, char **argv)
+{
+    int i, count = argc - 1;
+    int *nums = malloc(count * sizeof(int));
+    for (i = 0; i < count; i++) {
+        nums[i] = atoi(argv[i + 1]);
+    }
+    printf("%d\n", maxArea(nums, count));
+    return 0;
+}
