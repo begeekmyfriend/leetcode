769617debe81cfcc66cbc8085fcfdd0a44877e46
begeekmyfriend <begeekmyfriend@gmail.com>
2021-02-17 21:24:18 +0800

    Add C++ implementation
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- 0072_edit_distance/edit_distance.cc
+++ 0072_edit_distance/edit_distance.cc
@@ -5,25 +5,27 @@ using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp;
-        for (int i = 0; i <= word1.length(); i++) {
-            dp.push_back(vector<int>(word2.length() + 1));
-            dp[i][0] = i;
-        }
-        for (int i = 0; i <= word2.length(); i++) {
-            dp[0][i] = i;
+        int l1 = word1.length();
+        int l2 = word2.length();
+        vector<int> dp(l2 + 1);
+        for (int i = 0; i <= l2; i++) {
+            dp[i] = i;
         }
 
-        for (int i = 1; i <= word1.length(); i++) {
-            for (int j = 1; j <= word2.length(); j++) {
+        int up = 0;
+        for (int i = 1; i <= l1; i++) {
+            int left_up = dp[0];
+            dp[0] = i;
+            for (int j = 1; j <= l2; j++) {
+                 up = dp[j];
                  if (word1[i - 1] == word2[j - 1]) {
-                     dp[i][j] = dp[i - 1][j - 1];
+                     dp[j] = left_up;
                  } else {
-                     dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+                     dp[j] = 1 + min(left_up, min(up, dp[j - 1]));
                  }
+                 left_up = up;
             }
         }
-
-        return dp[word1.length()][word2.length()];
+        return dp[l2];
     }
 };
