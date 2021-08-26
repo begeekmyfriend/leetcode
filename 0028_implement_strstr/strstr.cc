e8d1f7c082ec0edd4dec63c433e6ef9a36aa8556
Pratik Goyal <pratikgoyal2712@gmail.com>
2021-08-26 19:22:50 +0530

    Updated strstr.cc
    Implemented Rabin-Karp Algorithm for 0028_implement_strstr

--- 0028_implement_strstr/strstr.cc
+++ 0028_implement_strstr/strstr.cc
@@ -4,7 +4,41 @@ using namespace std;
 
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        return haystack.find(needle);
-    }
+	
+	// Rabin-Karp Algorithm for string matching
+	int strStr(string haystack, string needle) {
+		long long mod = 1e9 + 7, p = 53;
+		int h = haystack.size(), n = needle.size();
+		if (h < n) return -1;
+		if (n == 0) return 0;
+
+		// precalculating powers of p
+		vector<long long> p_pow(max(h, n));
+		p_pow[0] = 1;
+		for (int i = 1; i < p_pow.size(); i++) {
+			p_pow[i] = (p_pow[i - 1] * p) % mod;
+		}
+		// calculating haystack_hash
+		vector<long long> haystack_hash(haystack.size() + 1, 0);
+		for (int i = 0; i < h; i++) {
+			haystack_hash[i + 1] = (haystack_hash[i] + (haystack[i] - 'a' + 1) * p_pow[i]) % mod;
+		}
+
+		// calculating needle_hash
+		long long needle_hash = 0;
+		for (int i = 0; i < n; i++) {
+			needle_hash = (needle_hash + (needle[i] - 'a' + 1) * p_pow[i]) % mod;
+		}
+
+		// checking for matching hashes
+		for (int i = 0; i <= h - n; i++) {
+			long long curr_hash = (haystack_hash[i + n] + mod - haystack_hash[i]) % mod;
+			if (curr_hash == (needle_hash * p_pow[i]) % mod) {
+				return i;
+			}
+		}
+
+		// if no hash is find
+		return -1;
+	}
 };
