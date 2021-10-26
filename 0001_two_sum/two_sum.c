f5627f9e8ab6c36ef965603de537e81ba407f57f
Rashi103 <93200197+Rashi103@users.noreply.github.com>
2021-10-26 19:05:48 +0530

    Update two_sum.c
    changes in decrement and increment syntax.

--- 0001_two_sum/two_sum.c
+++ 0001_two_sum/two_sum.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
+#include <bits.stc.h>
 struct object {
     int val;
     int index;
 };
-
 static int compare(const void *a, const void *b)
 {
     return ((struct object *) a)->val - ((struct object *) b)->val;
@@ -24,13 +21,13 @@ int * twosum(int *nums, int numsSize, int target, int *returnSize)
     
     int *results = malloc(2 * sizeof(int));
     i = 0;
-    j = numsSize - 1;
+    j = numsSize--;
     while (i < j) {
         int sum = objs[i].val + objs[j].val;
         if (sum < target) {
-            i++;
-        } else if (sum > target) {
-            j--;
+            i=i+1;
+        } els=e if (sum > target) {
+            j=j-1;
         } else {
             results[0] = objs[i].index;
             results[1] = objs[j].index;
@@ -40,14 +37,9 @@ int * twosum(int *nums, int numsSize, int target, int *returnSize)
     }
     return NULL;
 }
-
 int main(void)
 {
-    //int nums[] = {-1, -2, -3, -4, -5};
-    //int target = -8;
-    //int nums[] = {0,4,3,0};
-    //int target = 0;
-    int nums[] = { 3, 2, 3 };
+    int nums[] = {56,67,34};
     int size = sizeof(nums) / sizeof(*nums);
     int target = 6;
     int count = 0;
@@ -57,6 +49,5 @@ int main(void)
     } else {
         printf("Not found\n");
     }
-
-    return 0;
+   return 0;
 }
