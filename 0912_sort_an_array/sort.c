5af6c04a1b64b00ea5c54ac3d43d2d192d8f1ddc
begeekmyfriend <begeekmyfriend@gmail.com>
2024-02-21 11:58:52 +0800

    Improvement
    Signed-off-by: begeekmyfriend <begeekmyfriend@gmail.com>

--- 0912_sort_an_array/sort.c
+++ 0912_sort_an_array/sort.c
@@ -4,11 +4,11 @@
 
 static void show(int *nums, int lo, int hi)
 {
-        int i;
-        for (i = lo; i <= hi; i++) {
-                printf("%d ", nums[i]);
-        }
-        printf("\n");
+    int i;
+    for (i = lo; i <= hi; i++) {
+            printf("%d ", nums[i]);
+    }
+    printf("\n");
 }
 
 static inline void swap(int *a, int *b)
@@ -52,16 +52,70 @@ static void quick_sort(int *nums, int lo, int hi)
     quick_sort(nums, i + 1, hi);
 }
 
+static void merge(int *nums, int lo, int mid, int hi)
+{
+    int i, j, k, size = hi - mid;
+    int *tmp = malloc(size * sizeof(int));
+
+    for (j = 0; j < size; j++) {
+        tmp[j] = nums[mid + 1 + j];
+    }
+
+    i = mid;
+    j = size - 1;
+    k = hi;
+    while (i >= lo && j >= 0) {
+        if (tmp[j] >= nums[i]) {
+            nums[k--] = tmp[j--];
+        } else {
+            nums[k--] = nums[i--];
+        }
+    }
+
+    while (j >= 0) {
+        nums[k--] = tmp[j--];
+    }
+
+    free(tmp);
+}
+
+static void merge_sort(int *nums, int lo, int hi)
+{
+    int mid;
+
+    if (lo >= hi) {
+        return;
+    }
+
+    mid = lo + (hi - lo) / 2;
+
+    merge_sort(nums, lo, mid);
+    merge_sort(nums, mid + 1, hi);
+
+    merge(nums, lo, mid, hi);
+}
+
+int *sortArray(int *nums, int numsSize, int *returnSize)
+{
+#if 1
+    quick_sort(nums, 0, numsSize - 1);
+#else
+    merge_sort(nums, 0, numsSize - 1);
+#endif
+    *returnSize = numsSize;
+    return nums;
+}
+
 int main(int argc, char **argv)
 {
     int i, count = argc - 1;
+    int ret_size = 0;
     int *nums = malloc(count * sizeof(int));
     for (i = 0; i < count; i++) {
         nums[i] = atoi(argv[i + 1]);
     }
 
-    quick_sort(nums, 0, count - 1);
-    show(nums, 0, count - 1);
+    show(sortArray(nums, count, &ret_size), 0, ret_size - 1);
 
     return 0;
 }
